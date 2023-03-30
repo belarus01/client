17bb043794935055cd9e9c8a54180e5035fe411f
Sergey Miranovich <belarus1552@outlook.com>
2023-03-30 17:34:34 +0300

    !

--- src/api/http.api.ts
+++ src/api/http.api.ts
@@ -4,11 +4,11 @@ import { ApiError } from '@app/api/ApiError';
 import { readToken } from '@app/services/localStorage.service';
 
 export const httpApi = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: 'http://192.168.150.183:4000',
 });
 
 httpApi.interceptors.request.use((config) => {
-  config.headers = { ...config.headers, Authorization: `Bearer ${readToken()}` };
+  config.headers = {Authorization: `Bearer ${readToken()}` };
 
   return config;
 });
--- src/components/planning/Planning.tsx
+++ src/components/planning/Planning.tsx
@@ -7,9 +7,16 @@ import ruLocale from '@fullcalendar/core/locales/ru'
 import { Card } from '../common/Card/Card';
 import { useTranslation } from 'react-i18next';
 import { Alert } from '../common/Alert/Alert';
+import {useState} from 'react';
+import {useEffect} from 'react';
+import axios from 'axios';
+import { httpApi } from '@app/api/http.api';
+import { Modal } from '../common/Modal/Modal';
+//import { httpApi } from '@app/api/http.api';
 
 export const Planning: React.FC = () => {
     const { t } = useTranslation();
+    const [data, setData] = useState<any[]>([]);
 
     const onDateClick = (info:any) =>{
         const date = info.start;
@@ -19,8 +26,15 @@ export const Planning: React.FC = () => {
     const onSelect = (info:any) =>{
         alert('selected ' + info.startStr + ' to ' + info.endStr);
     }
+
+    useEffect(()=>{
+        const res = httpApi.get<any>('events/get/all/userId=1').then((e:any)=>
+        {
+            setData(e.data);
+            console.log(e.data);
+        })
+    },[])
     return (
-             
             <FullCalendar
                 plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
                 initialView='dayGridMonth'
@@ -30,12 +44,24 @@ export const Planning: React.FC = () => {
                     center: 'title',
                     right: 'dayGridMonth,timeGridWeek,timeGridDay'
                 }}
+                editable={true}
                 dateClick={onDateClick}
                 select={onSelect}
                 selectable={true}
-                
-            />
-       
+                eventDrop={function(info){
+                    data.map((element)=>{
+                        console.log(element.title);
+                    })
+                    alert(data.find(element=>element.title == info.event.title))
+                    if(!confirm(info.event.title)){
+                        info.revert();
+                    }
+                }}
 
+                // eventResize={function(info){
+                //     if(Modal.confirm)
+                // }}
+                events={data} 
+            />
     )
 }
