3e81f941cc06d02fdea5c6fcb5af6555ad7658d8
Sergey Miranovich <belarus1552@outlook.com>
2023-04-17 05:55:28 +0300

    refactor update password form

--- src/components/users/forms/UpdatePasswordForm.tsx
+++ src/components/users/forms/UpdatePasswordForm.tsx
@@ -10,49 +10,37 @@ import * as S from './UserForms.styles';
 import { Modal } from '@app/components/common/Modal/Modal';
 import { InputPassword } from '@app/components/common/inputs/InputPassword/InputPassword';
 
-interface UpdatePasswordFormProps {
-    open: boolean;
-    onCancel: () => void;
-}
 
-export const UpdatePasswordForm: React.FC<UpdatePasswordFormProps> = ({open, onCancel}) => {
+export const UpdatePasswordForm: React.FC = () => {
   const [isFieldsChanged, setFieldsChanged] = useState(false);
   const [isLoading, setLoading] = useState(false);
 
-
+  //todo: add validation and sending
   const onFinish = (values: []) => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
       setFieldsChanged(false);
-      notificationController.success({ message: ''});
+      notificationController.success({ message: 'Пароль обновлен' });
       console.log(values);
     }, 1000);
   };
 
+  return (
+    <>
+      <BaseButtonsForm
+        layout="vertical"
+        onFinish={onFinish}
+        isFieldsChanged={false}
+      >
+        <BaseButtonsForm.Item label="Новый пароль" name="pas1">
+          <InputPassword />
+        </BaseButtonsForm.Item>
+        <BaseButtonsForm.Item label="Повторите ввод пароля" name="pas2">
+          <InputPassword />
+        </BaseButtonsForm.Item>
+      </BaseButtonsForm>
+    </>
 
-    return (
-        <Modal
-            closable
-            footer={null}
-            onCancel={onCancel}
-            destroyOnClose
-            title={'Изменение пароля'}
-            centered
-            open={open}
-        >
-            <BaseButtonsForm
-                layout="vertical"
-                onFinish={onFinish}
-                 isFieldsChanged={false}
-            >
-                <BaseButtonsForm.Item label="Новый пароль" name="pas1">
-                    <InputPassword />
-                </BaseButtonsForm.Item>
-                <BaseButtonsForm.Item label="Повторите ввод пароля" name="pas2">
-                    <InputPassword />
-                </BaseButtonsForm.Item>
-            </BaseButtonsForm>
-            </Modal>
   );
 };
